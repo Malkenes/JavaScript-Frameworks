b2894f09846b1e752e18287eafb056e248bfbc63
Malkenes <mmalkenes@hotmail.com>
2024-09-17 14:20:22 +0200

    add footer

---
+++ src/components/footer.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+background: #455B7D;
+color: white;
+display: flex;
+flex-wrap: wrap;
+gap: 40px;
+padding: 32px;
+div {
+    text-align: start;
+}
+ul{
+list-style: none;
+padding: 0;
+}
+li {
+    margin-bottom: 12px; 
+}
+`
